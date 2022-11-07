5dddac0bb6a21c697ffa2b8adc44ea7592ae8319
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2022-11-07 07:16:47 -0600

    commit4

--- lib/custom_widgets/product_card.dart
+++ lib/custom_widgets/product_card.dart
@@ -24,22 +24,35 @@ class _ProductCardState extends State<ProductCard> {
       child: Container(
         margin: EdgeInsets.all(4),
         child: Stack(children: [
-          // Image.network(
-          //   widget.imageUrl,
-          //   fit: BoxFit.cover,
-          // ),
-
+          Image.network(
+            widget.imageUrl,
+            fit: BoxFit.cover,
+          ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              padding: EdgeInsets.all(8),
+              padding: EdgeInsets.all(12),
               width: double.infinity,
-              height: 60,
-              color: Color.fromARGB(108, 0, 0, 0),
+              height: 65,
+              color: Color.fromARGB(88, 0, 0, 0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [Text(widget.name), Text(widget.price.toString())],
+                children: [
+                  Text(
+                    widget.name,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    widget.price.toString(),
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 184, 119, 0),
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
               ),
             ),
           )
